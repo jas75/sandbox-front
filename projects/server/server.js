70f1de37107a39c9d86d999c8dc878729a071e22
jas75 <azoulayjason@gmail.com>
2020-09-01 11:52:57 +0200

    feat: mongoose implementation

--- projects/server/server.js
+++ projects/server/server.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const express = require('express');
 const compression = require('compression');
+const config = require('./src/config/config');
+const mongoose = require('mongoose');
 
 const CONTEXT = `/${process.env.CONTEXT || 'angular-ngrx-material-starter'}`;
 const PORT = process.env.PORT || 4000;
@@ -23,3 +25,27 @@ app.use(
 app.listen(PORT, () =>
   console.log(`App running on http://localhost:${PORT}${CONTEXT}`)
 );
+app.get('/test', (req, res) => {
+  return res.send('Hello! The API is at http://localhost:3000/api');
+});
+
+// DB connection
+let mongoUri = config.mongoUri;
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+});
+const connection = mongoose.connection;
+
+connection.once('open', () => {
+  console.log('MongoDB connection established successfully');
+});
+
+connection.on('error', (err) => {
+  console.log(
+    'MongoDb connection error. Please make sure MongoDB is running. ' + err
+  );
+  process.exit();
+});
