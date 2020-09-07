4b4434bf01f7bd5784b19b1ce143063520716207
jas75 <azoulayjason@gmail.com>
2020-09-07 11:00:39 +0200

    feat: set login form

--- projects/angular-ngrx-material-starter/src/app/features/home/login/login.component.ts
+++ projects/angular-ngrx-material-starter/src/app/features/home/login/login.component.ts
@@ -7,6 +7,7 @@ import {
 } from '@angular/core';
 
 import { ROUTE_ANIMATIONS_ELEMENTS } from '../../../core/core.module';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'anms-login',
@@ -16,14 +17,36 @@ import { ROUTE_ANIMATIONS_ELEMENTS } from '../../../core/core.module';
 })
 export class LoginComponent implements OnInit {
   routeAnimationsElements = ROUTE_ANIMATIONS_ELEMENTS;
+  loginForm: FormGroup;
 
   @Output() formType = new EventEmitter<boolean>();
 
-  constructor() {}
+  constructor(private formBuilder: FormBuilder) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.createForm();
+  }
 
   public switchForm(): void {
     this.formType.emit(false);
+  }
+
+  public onSubmit() {
+    console.log('wesh');
+  }
+
+  public createForm(): void {
+    this.loginForm = this.formBuilder.group({
+      email: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.pattern(
+            '^\\w+([\\.-]?\\w+)*@\\w+([\\.-]?\\w+)*(\\.\\w{2,3})+$'
+          )
+        ])
+      ],
+      password: ['', Validators.required]
+    });
   }
 }
